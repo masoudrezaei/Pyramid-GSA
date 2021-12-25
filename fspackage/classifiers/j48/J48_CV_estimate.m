6882c10b60a893ee23b85157765bc27c5d3f65ef
masoudrezaei <masoudrezaei082@gmail.com>
2021-12-25 10:02:13 +0330

    fspackage
    we have used the fspackage in this project.

--- fspackage/classifiers/j48/J48_CV_estimate.m
+++
@@ -1,4 +0,0 @@
-function [tsACC] = J48_CV_estimate(trainX,trainY,testX,testY,options)
-
-tsACC = j48(options, trainX, trainY, testX, testY);
-tsACC = tsACC.tree_accuracy;
--- fspackage/fs_sup_blogreg/make.m
+++
@@ -1,1 +0,0 @@
-mex -largeArrayDims blogreg.c
